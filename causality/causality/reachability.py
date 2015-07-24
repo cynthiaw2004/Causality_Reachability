02be8a5376d1c821f6522980e154633dcd13e0c8
Ian Beaver <ibeaver@nextit.com>
2015-07-24 15:26:30 -0600

    Local distributed version of create_domain done

--- causality/causality/reachability.py
+++ causality/causality/reachability.py
@@ -1,12 +1,14 @@
 from __future__ import print_function, unicode_literals, division, absolute_import
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from copy import copy
 import cStringIO
 import gzip
-from itertools import permutations, product, combinations, chain, izip, repeat
+from itertools import permutations, product, combinations, chain, izip, repeat, islice
 import json
 from multiprocessing import Pool, cpu_count
+import os
+import shutil
 import sys
+from tempfile import NamedTemporaryFile
 import time
 
 global _DEBUG
@@ -53,27 +55,30 @@ def generate_empty_graph(n):
     return g
 
 
+def grouper(n, iterable):
+    """ http://stackoverflow.com/a/8991553 """
+    it = iter(iterable)
+    while True:
+        chunk = tuple(islice(it, n))
+        if not chunk:
+            return
+        yield chunk
+
+
 def generate_partial(args):
     """ Process entry point, converts a subset of graphs to dictionary form """
-    subgraphs = args[0]
-    n = args[1]
-    g = generate_empty_graph(n)
-    # Store graphs in gzipped memory
-    gz_mem_file = cStringIO.StringIO()
-    gzip_obj = gzip.GzipFile(mode='ab', fileobj=gz_mem_file)
-    for e in subgraphs:
-        e = map(str, e)
-        try:
-            g[e[0]][e[1]].add((0, 1))
-        except KeyError:
-            g[e[0]][e[1]] = set([(0, 1)])
-    gzip_obj.write("---\n{}...\n".format(json.dumps([g], default=handler)))
+    graph_iter, n, outdir = args
+    # Write graphs out to gzipped file
+    gz_file = NamedTemporaryFile(suffix=".gz", dir=outdir, delete=False)
+    gzip_obj = gzip.GzipFile(mode='wb', fileobj=gz_file)
+    for subgraphs in graph_iter:
+        gzip_obj.write("---\n{}\n...\n".format(subgraphs))
     gzip_obj.close()
-    return gz_mem_file.getvalue()
+    gz_file.close()
 
 
 @ConditonalProfile
-def create_domain(n, nprocs):
+def create_domain(n, nprocs, outdir):
     """ input: number of vertices
         output: list of all possible ground truths with n nodes
         (only single directed edges allowed in domain graphs) """
@@ -81,48 +86,26 @@ def create_domain(n, nprocs):
     # determine all single directed edges
     single_directed_edge_list = list(product(vertices, vertices))
     # determine all possible graphs that can be formed
-    allgraphs = chain.from_iterable(combinations(single_directed_edge_list, r)
-                                    for r in xrange(len(vertices) ** 2 + 1))
+    graph_count = len(vertices) ** 2
+    allgraphs = chain.from_iterable(combinations(single_directed_edge_list, r) for r in xrange(graph_count + 1))
     if nprocs != -1:
         # Run locally multiprocess
         if nprocs == 0:
             nprocs = cpu_count()
         pool = Pool(processes=nprocs)
-        gz_mem_file = cStringIO.StringIO()
-        for res in pool.imap(generate_partial, izip(allgraphs, repeat(n)), chunksize=1):
-            gz_mem_file.write(res)
+        num_tasks = 10000  # max number of SGE tasks and output files
+        chunk_size = max(graph_count / num_tasks, 20000)  # Dont starve workers
+        for res in pool.imap(generate_partial, izip(grouper(chunk_size, allgraphs), repeat(n), repeat(outdir))):
+            pass
         pool.close()
-
     else:
-        # Run locally as single process
-        # now to convert to dictionary form that sergey uses
-        g_base = generate_empty_graph(n)
-        g = copy(g_base)
-        # Store graphs in gzipped memory
-        gz_mem_file = cStringIO.StringIO()
-        gzip_obj = gzip.GzipFile(mode='wb', fileobj=gz_mem_file)
-        chunk_size = 10000  # how much to store in mem before flushing
-        glist = []
-        for subgraphs in allgraphs:
-            # split out procs here
-            for e in subgraphs:
-                e = map(str, e)
-                try:
-                    g[e[0]][e[1]].add((0, 1))
-                except KeyError:
-                    g[e[0]][e[1]] = set([(0, 1)])
-            glist.append(g)
-            if len(glist) >= chunk_size:
-                gzip_obj.write("---\n{}...\n".format(json.dumps(glist, default=handler)))
-                glist = []
-            g = copy(g_base)
-        gzip_obj.close()
+        generate_partial((allgraphs, n, outdir))
 
-    return gz_mem_file
+    return outdir
 
 
 @ConditonalProfile
-def create_codomain(n):
+def create_codomain(n, nprocs, outdir):
     """ input: number of vertices
         output: the codomain-list of all possible graphs with n many vertices
         (both directed and bidirected edges allowed)
@@ -217,7 +200,7 @@ if __name__ == "__main__":
                             formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-n", type=int, help='Number of nodes in graph')
     parser.add_argument("--profile", help='Print profiling info', action='store_true')
-    # parser.add_argument('-out', help='Output file to store results in (in zickle format)', type=str, required=True)
+    parser.add_argument('-out', help='Output directory to store results in', type=str, required=True)
     group1 = parser.add_mutually_exclusive_group(required=True)
     group1.add_argument('--domain', help='Generate domain given N', action='store_true')
     group1.add_argument('--codomain', help='Generate co-domain given N', action='store_true')
@@ -230,21 +213,31 @@ if __name__ == "__main__":
 
     print("time: {}\nargs: {}".format(datetime.datetime.now(), args.__dict__))
 
-    _DEBUG = args.profile if args.nprocs == -1 else False
+    try:
+        if os.path.exists(args.out):
+            shutil.rmtree(args.out)
+        os.makedirs(args.out)
+    except OSError, e:
+        sys.exit("Cannot create output directory {}: {}".format(args.out, e))
+
+    _DEBUG = False
+    if args.profile:
+        if args.nprocs == -1:
+            _DEBUG = args.profile
+        else:
+            print("Profiling is not availible in distributed mode.")
 
     if args.domain:
         if args.n is None:
             sys.exit("You must specify n")
+
         t0 = time.time()
-        graphs = create_domain(args.n, args.nprocs)
-        # print("Graph count: {}\nGraph size: {} Bytes\nAvg. Graph size: {} Bytes\n\n".format(
-        #    len(graphs), sys.getsizeof(graphs), sys.getsizeof(graphs) / len(graphs)))
-        # print("Results in {}".format(filename))
-        print("Exec Time: {} Seconds\nGraph size: {} MB".format(time.time() - t0, sys.getsizeof(graphs.getvalue())/(1024**2)))
+        create_domain(args.n, args.nprocs, args.out)
+        print("Exec Time: {} Seconds".format(time.time() - t0))
 
     if args.codomain:
         if args.n is None:
             sys.exit("You must specify n")
-        graphs = create_codomain(args.n)
-        print("Graph count: {}\nGraph size: {} Bytes\nAvg. Graph size: {} Bytes\n\n".format(
-            len(graphs), sys.getsizeof(graphs), sys.getsizeof(graphs) / len(graphs)))
+        t0 = time.time()
+        graphs = create_codomain(args.n, args.nprocs, args.out)
+        print("Exec Time: {} Seconds".format(time.time() - t0))
